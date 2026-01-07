a0dc1004260434368f99e07ab5277280a90f0057
notestnocode <167581989+notestnocode@users.noreply.github.com>
2026-01-07 15:22:35 -0600

    Update app.py
    models/gemini-2.5-flash

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if "GOOGLE_API_KEY" not in st.secrets:
 genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 # Inicializar el modelo
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.5-flash')
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
